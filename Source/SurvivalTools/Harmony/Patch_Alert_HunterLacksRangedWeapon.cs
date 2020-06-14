feea3b6eaca8a74724f9474a869376bf4cd5a94c
LecrisUT <lecris.phy@gmail.com>
2020-06-14 14:19:08 +0900

    Fixed Hunter alert when working with tools

---
+++ Source/SurvivalTools/Harmony/Patch_Alert_HunterLacksRangedWeapon.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Verse;
+using Verse.AI;
+
+namespace SurvivalTools.HarmonyPatches
+{
+    [HarmonyPatch]
+    public static class Patch_Alert_HunterLacksRangedWeapon
+    {
+        public static MethodBase TargetMethod()
+            => AccessTools.PropertyGetter(typeof(Alert_HunterLacksRangedWeapon), "HuntersWithoutRangedWeapon");
+        public static void Postfix(ref List<Pawn> __result)
+        {
+            __result.RemoveAll(t => t.GetToolTracker().drawTool);
+        }
+    }
+}
